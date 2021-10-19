e3bd0dc4302f15ba870967fe7100043110b48dc3
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-19 13:45:12 -0400

    javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form")
+console.log(form);
+
+const newSong = document.getElementById("new-song")
+console.log(newSong)
+
+const newArtist = document.getElementById("new-artist")
+console.log(newArtist)
+
+const container = document.getElementById("song-container")
+console.log(container);
+
+const url = ""
