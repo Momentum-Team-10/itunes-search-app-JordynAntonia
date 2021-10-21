05335ec35fe4f787be8a5d3db0d55645c07f3fb9
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-21 10:17:17 -0400

    added more JS

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const nextButton = document.getElementById(`next-button`)
 
 // getting the music for player
 const audioPlayer = document.getElementById(`audio-player`);
-const url = "https://itunes.apple.com/search?term=.";
+const url = "https://itunes.apple.com/search?parameterkeyvalue.";
 
 
 // within the "form" we want to have a list of songs based on a certain Artist, copying the structure that is already on the CSS.
@@ -25,10 +25,11 @@ form.addEventListener("submit", (e) => {
         method:'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
-            // when the user clicks submit, the results should sort "cards" of the album, song title and the artist.
-            album: artistAlbum,
-            title: songTitle.sort,
-            artist: songArtist,
+            // when the user clicks submit, the results should sort "cards" of the album, song title and the artist. Terms from Itunes search document.
+            album: album,
+            title: musicTrack.value,
+            artist: musicArtist,
+            
         })
     })
     // promises/respose/data/json
@@ -44,16 +45,28 @@ fetch(url)
 // taking the info from the body above to render functions and add the container
         for(let item of data) {
             renderMusicCard(item)
-            let musicCard = document.createElement("div")
-            renderAlbum(item,musicCard);
-            renderTitle(item,musicCard);
-            renderArtist(item,musicCard);
-            container.appendChild(musicCard);
+            // let musicCard = document.createElement("div")
+            // renderAlbum(item, musicCard);
+            // renderTitle(item, musicCard);
+            // renderArtist(item, musicCard);
+            // container.appendChild(musicCard);
 
         }
+    };
     
         function renderMusicCard(item) {
             // how all of the items are displayed on each "card"
             let musicCard = document.createElement("div");
+            musicCard.classList.add("musicCard")
+            renderAlbum(item, musicCard);
+            renderTitle(item, musicCard);
+            renderArtist(item, musicCard);
+            deleteMusicCard(musicCard);
+            container.appendChild(musicCard);
 
+
+        }
+
+        function deleteMusicCard(musicCard) {
+            let deletemusicCard = document.createElement("sort");
         }
