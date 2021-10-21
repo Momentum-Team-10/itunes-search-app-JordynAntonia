f735dbbf5d7b10e992807aaec3323ad4d4d8225a
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-20 23:02:42 -0400

    working on the JS

--- main.js
+++ main.js
@@ -1,31 +1,54 @@
-// creating the element's
+// getting elements
 const form = document.getElementById("form")
-console.log(form);
+const songImage = document.getElementById(`song-image`)
+const songTitle = document.getElementById(`song-title`)
+const songArtist = document.getElementById(`song-artist`)
+const artistAlbum = document.getElementById(`artist-album`)
+const songNext = document.getElementById(`song-next-up`)
+const container = document.getElementById(`song-container`)
 
-const newSong = document.getElementById("new-song")
-console.log(newSong);
 
-const newArtist = document.getElementById("new-artist")
-console.log(newArtist);
+// getting buttons 
+const previousButton = document.getElementById(`previous-button`)
+const playButtonIcon = document.getElementById(`play-icon`)
+const playButton = document.getElementById(`play-button`)
+const nextButton = document.getElementById(`next-button`)
 
-const newAlbum = document.getElementById("new-album")
-console.log(newAlbum);
+// getting the music for player
+const audioPlayer = document.getElementById(`audio-player`);
+const url = "https://itunes.apple.com/search?term=.";
 
-const bandName = document.getElementById("band-name")
-console.log(bandName);
 
-const container = document.getElementById("song-container")
-console.log(container);
-
-const url = "http://127.0.0.1:8080 https://itunes.apple.com/search?term=.";
-
-// getting JSON from the Database
+// within the "form" we want to have a list of songs based on a certain Artist, copying the structure that is already on the CSS.
+form.addEventListener("submit", (e) => {
+    fetch(url, {
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            // when the user clicks submit, the results should sort "cards" of the album, song title and the artist.
+            album: artistAlbum,
+            title: songTitle.sort,
+            artist: songArtist,
+        })
+    })
+    // promises/respose/data/json
+    .then(res => res.json())
+    .then(data => {
+        renderMusicCard(data)
+    })
+})
 
 fetch(url)
-.then((response) => response.json())
-.then((data) => {
-    console.log(data);
+    .then(res) => res.json())
+    .then(data) => {
+// taking the info from the body above to render functions and add the container
+        for(let item of data) {
+            renderMusicCard(item)
+            let musicCard = document.createElement("div")
+            renderAlbum(item,musicCard);
+            renderTitle(item,musicCard);
+            renderArtist(item,musicCard);
+            container.appendChild(musicCard);
 
-for(let item of data) {
-    // create div containers 
-}
+        }
+    }
